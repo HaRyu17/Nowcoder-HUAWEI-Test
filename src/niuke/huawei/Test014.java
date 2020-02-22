7508b74e0719b8145c3bb00c9ff33c1014b2bb64
HaRyu17 <153730693@qq.com>
2020-02-22 16:26:44 +0800

    2020/02/22

--- src/niuke/huawei/Test014.java
+++ src/niuke/huawei/Test014.java
@@ -4,5 +4,18 @@ package niuke.huawei;
  * @author Horace
  * @date 2020/2/22 - 11:00
  */
+import java.util.*;
 public class Test014 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        ArrayList<String> setst = new ArrayList<String>();
+        while(sc.hasNext()){
+            setst.add(sc.next());
+        }
+        Collections.sort(setst);
+        for(String srt:setst){
+            System.out.println(srt);
+        }
+    }
 }
--- src/niuke/huawei/Test015.java
+++ src/niuke/huawei/Test015.java
@@ -4,5 +4,19 @@ package niuke.huawei;
  * @author Horace
  * @date 2020/2/22 - 11:00
  */
+import java.util.Scanner;
 public class Test015 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        String bs = Integer.toBinaryString(num);
+        char[] bc = bs.toCharArray();
+        int out = 0;
+        for(int i = 0; i < bc.length; i++){
+            if(bc[i] == '1'){
+                out++;
+            }
+        }
+        System.out.println(out);
+    }
 }
