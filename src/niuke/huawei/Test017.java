0151c9d9b8fe018c1e3f82334d7a8f3f0cda6f8d
HaRyu17 <153730693@qq.com>
2020-02-22 16:58:03 +0800

    2020/02/22

--- src/niuke/huawei/Test017.java
+++ src/niuke/huawei/Test017.java
@@ -4,5 +4,36 @@ package niuke.huawei;
  * @author Horace
  * @date 2020/2/22 - 11:00
  */
+import java.util.*;
 public class Test017 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()){
+            String input = sc.nextLine();
+            String[] is = input.split(";");
+            int x = 0;
+            int y = 0;
+            for(int i = 0; i<is.length; i++){
+                try{
+                    if(is[i].charAt(0) == 'A' && is[i].substring(1).matches("[0-9]+")){
+                        x -= Integer.parseInt(is[i].substring(1));
+                    }
+                    if(is[i].charAt(0) == 'S' && is[i].substring(1).matches("[0-9]+")){
+                        y -= Integer.parseInt(is[i].substring(1));
+                    }
+                    if(is[i].charAt(0) == 'W' && is[i].substring(1).matches("[0-9]+")){
+                        y += Integer.parseInt(is[i].substring(1));
+                    }
+                    if(is[i].charAt(0) == 'D' && is[i].substring(1).matches("[0-9]+")){
+                        x += Integer.parseInt(is[i].substring(1));
+                    }
+                }catch(Exception e){
+                    continue;
+                }
+
+            }
+            System.out.println(x+","+y);
+        }
+
+    }
 }
