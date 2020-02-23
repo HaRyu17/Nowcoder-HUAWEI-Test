38c4d17a18ecb8cec071ae897fa04d6e917e4867
HaRyu17 <153730693@qq.com>
2020-02-23 17:03:32 +0800

    2020/02/23

--- src/niuke/huawei/Test019.java
+++ src/niuke/huawei/Test019.java
@@ -4,5 +4,37 @@ package niuke.huawei;
  * @author Horace
  * @date 2020/2/22 - 11:00
  */
+import java.util.Scanner;
+import java.util.Map;
+import java.util.LinkedHashMap;
 public class Test019 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Map<String,Integer> fm = new LinkedHashMap<String,Integer>();
+
+        while(sc.hasNext()){
+            String folder = sc.next();
+            int num = sc.nextInt();
+            String[] sa = folder.split("\\\\");
+            String fn = sa[sa.length-1];
+            if(sa[sa.length-1].length()>16){
+                fn = fn.substring(fn.length()-16);
+            }
+            String out = fn + " " + num;
+            if(fm.get(out) == null){
+                fm.put(out,1);
+            }else if(fm.get(out) != null){
+                int numout = fm.get(out)+1;
+                fm.put(out,numout);
+            }
+        }
+        int count = 0;
+        for(String key:fm.keySet()){
+            count++;
+            if(count >(fm.keySet().size()-8))
+                System.out.println(key + " " + fm.get(key));
+        }
+
+    }
+
 }
