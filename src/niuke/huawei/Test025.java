1dcfd66a4b08a099f3ae3737a4e0365a7ccfee9c
HaRyu17 <153730693@qq.com>
2020-02-24 16:48:41 +0800

    2020/02/24

--- src/niuke/huawei/Test025.java
+++ src/niuke/huawei/Test025.java
@@ -4,5 +4,28 @@ package niuke.huawei;
  * @author Horace
  * @date 2020/2/23 - 21:37
  */
+import java.util.Scanner;
 public class Test025 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            String st = sc.nextLine();
+            char[] stc = st.toCharArray();
+            StringBuffer sb = new StringBuffer();
+            for(int i = 0;i<26;i++){
+                char c = (char)(i+'A');
+                for(int j=0;j<stc.length;j++){
+                    if(stc[j] == c||stc[j] == (char)(c+32)){
+                        sb.append(stc[j]);
+                    }
+                }
+            }
+            for(int k = 0;k<stc.length;k++){
+                if(!(stc[k]>='a'&&stc[k]<='z'||stc[k]>='A'&&stc[k]<='Z')){
+                    sb.insert(k,stc[k]);
+                }
+            }
+            System.out.println(sb.toString());
+        }
+    }
 }
