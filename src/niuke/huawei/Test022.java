8de952778ad8bf1a9fceece21f068cc5478681c8
HaRyu17 <153730693@qq.com>
2020-02-24 11:49:04 +0800

    2020/02/24

--- src/niuke/huawei/Test022.java
+++ src/niuke/huawei/Test022.java
@@ -4,5 +4,18 @@ package niuke.huawei;
  * @author Horace
  * @date 2020/2/23 - 21:37
  */
+import java.util.Scanner;
 public class Test022 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int num = sc.nextInt();
+            if(num == 0){
+                continue;
+            }
+            int cola = num/2;
+            System.out.println(cola);
+        }
+
+    }
 }
--- src/niuke/huawei/Test023.java
+++ src/niuke/huawei/Test023.java
@@ -4,5 +4,33 @@ package niuke.huawei;
  * @author Horace
  * @date 2020/2/23 - 21:37
  */
+import java.util.Scanner;
 public class Test023 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            String input = sc.next();
+            if(input.length()>20){
+                continue;
+            }
+            char[] ca = input.toCharArray();
+            int[] num = new int[26];
+            int min = Integer.MAX_VALUE;
+            for(int i = 0;i<ca.length;i++){
+                num[ca[i]-'a']++;
+            }
+            for(int i = 0;i<num.length;i++){
+                if(num[i]!=0&&min>num[i]){
+                    min = num[i];
+                }
+            }
+            for(int i = 0;i<ca.length;i++){
+                if(num[ca[i]-'a']!=min){
+                    System.out.print(ca[i]);
+                }
+            }
+            System.out.println();
+
+        }
+    }
 }
