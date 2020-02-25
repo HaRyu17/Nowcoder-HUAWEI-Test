c2e82f6c826884fe59fb9dc295795dcf94f6870b
HaRyu17 <153730693@qq.com>
2020-02-25 21:50:56 +0800

    最优解下次再整

--- src/niuke/huawei/Test034.java
+++ src/niuke/huawei/Test034.java
@@ -4,5 +4,28 @@ package niuke.huawei;
  * @author HaRyu17
  * @date 2020/2/23 - 21:37
  */
+import java.util.Scanner;
 public class Test034 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int num = sc.nextInt();
+
+            int start = 1;
+            int j = 2;
+            for(int i = 0;i<num;i++){
+                start +=i;
+                System.out.print(start);
+                int re = j+start;
+                for(int loop = j;loop<=num;loop++){
+                    System.out.print(" ");
+                    System.out.print(re);
+                    re +=loop+1;
+                }
+                j++;
+                System.out.println("");
+            }
+        }
+    }
+
 }
