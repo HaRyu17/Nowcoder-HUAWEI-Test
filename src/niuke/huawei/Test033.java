d9764695dde67b3b0906b7c630a2482ece68bc8e
HaRyu17 <153730693@qq.com>
2020-02-25 21:15:58 +0800

    图片整理

--- src/niuke/huawei/Test033.java
+++ src/niuke/huawei/Test033.java
@@ -4,5 +4,16 @@ package niuke.huawei;
  * @author HaRyu17
  * @date 2020/2/23 - 21:37
  */
+import java.util.Scanner;
+import java.util.Arrays;
 public class Test033 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            String input = sc.nextLine();
+            char[] c = input.toCharArray();
+            Arrays.sort(c);
+            System.out.println(c);
+        }
+    }
 }
