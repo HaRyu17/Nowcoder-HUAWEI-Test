60bda6b57bd2b8d8464e600a280c73038f53ec7a
HaRyu17 <153730693@qq.com>
2020-02-25 21:00:24 +0800

    注意int边界

--- src/niuke/huawei/Test032.java
+++ src/niuke/huawei/Test032.java
@@ -4,5 +4,49 @@ package niuke.huawei;
  * @author HaRyu17
  * @date 2020/2/23 - 21:37
  */
+import java.util.Scanner;
 public class Test032 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            String IP = sc.nextLine();
+            String enIP = sc.nextLine();
+            long num = code(IP);
+            String unIP = unentrypt(enIP);
+            System.out.println(num);
+            System.out.println(unIP);
+        }
+    }
+    public static long code(String s){
+        String[] ss = s.split("\\.");
+        String[] str = new String[4];
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0;i<ss.length;i++){
+            str[i] = Long.toBinaryString(Integer.parseInt(ss[i]));
+            while(str[i].length()<8){
+                str[i] = "0"+str[i];
+            }
+
+        }
+        for(int i = 0;i<str.length;i++){
+            sb.append(str[i]);
+        }
+        long num = Long.parseLong(sb.toString(),2);
+        return num;
+    }
+    public static String unentrypt(String n){
+        Long ln = Long.parseLong(n);
+        StringBuffer sb = new StringBuffer();
+        sb.append(Long.toBinaryString(ln));
+        String sbs = sb.toString();
+        while(sbs.length()<32){
+            sbs = "0"+sbs;
+        }
+        int a = Integer.parseUnsignedInt(sbs.substring(0,8),2);
+        int b = Integer.parseUnsignedInt(sbs.substring(8,16),2);
+        int c = Integer.parseUnsignedInt(sbs.substring(16,24),2);
+        int d = Integer.parseUnsignedInt(sbs.substring(24),2);
+        String s = a+"."+b+"."+c+"."+d;
+        return s;
+    }
 }
