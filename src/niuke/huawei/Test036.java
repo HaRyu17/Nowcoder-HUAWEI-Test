80791eaad615f978d5a7e851946216b560e65136
HaRyu17 <153730693@qq.com>
2020-02-26 11:52:02 +0800

    递增数列

--- src/niuke/huawei/Test036.java
+++ src/niuke/huawei/Test036.java
@@ -4,5 +4,23 @@ package niuke.huawei;
  * @author HaRyu17
  * @date 2020/2/23 - 21:37
  */
+import java.util.Scanner;
 public class Test036 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int month = sc.nextInt();
+            int one = 1;
+            int two = 0;
+            int three = 0;
+            while(--month!=0){
+                three += two;
+                two = one;
+                one = three;
+            }
+            int count = one + two + three;
+            System.out.println(count);
+        }
+
+    }
 }
