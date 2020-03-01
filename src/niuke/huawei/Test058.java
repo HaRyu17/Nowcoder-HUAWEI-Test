25fb4aba50f3724ec46859cbd7feeaefd17bdf5c
HaRyu17 <153730693@qq.com>
2020-03-01 22:03:09 +0800

    2020/03/01

--- src/niuke/huawei/Test058.java
+++ src/niuke/huawei/Test058.java
@@ -4,5 +4,26 @@ package niuke.huawei;
  * @author HaRyu17
  * @date 2020/2/29 - 16:54
  */
+
+import java.util.Scanner;
 public class Test058 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            String input = sc.next();
+            char[] arrCh = input.toCharArray();
+            boolean output = true;
+            for (char ch : arrCh) {
+                if (input.indexOf(ch) == input.lastIndexOf(ch)) {
+                    System.out.println(ch);
+                    output = false;
+                    break;
+                }
+            }
+            if(output){
+                System.out.println(-1);
+            }
+        }
+        sc.close();
+    }
 }
