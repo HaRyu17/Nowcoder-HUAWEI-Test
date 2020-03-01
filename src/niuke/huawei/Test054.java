55f571bf5c93c2bb71d6c7be017bd13ebeb41e36
HaRyu17 <153730693@qq.com>
2020-03-01 21:18:58 +0800

    2020/03/01

--- src/niuke/huawei/Test054.java
+++ src/niuke/huawei/Test054.java
@@ -4,5 +4,31 @@ package niuke.huawei;
  * @author HaRyu17
  * @date 2020/2/29 - 16:54
  */
+import java.util.Scanner;
 public class Test054 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int num = sc.nextInt();
+            if(num>30000){
+                continue;
+            }
+            int count = 0;
+            for(int i = 1;i<=num;i++){
+                if(i%7 == 0){
+                    count++;
+                }else if(i%10 == 7){
+                    count++;
+                    //反过来先求余再除有一个试例无法通过多1.
+                }else if((i/10)%10 == 7){
+                    count++;
+                }else if((i/100)%10 == 7){
+                    count++;
+                }else if((i/1000)%10 == 7){
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
 }
--- src/niuke/huawei/Test055.java
+++ src/niuke/huawei/Test055.java
@@ -4,5 +4,31 @@ package niuke.huawei;
  * @author HaRyu17
  * @date 2020/2/29 - 16:54
  */
+import java.util.Scanner;
 public class Test055 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int num = sc.nextInt();
+            if(num<=0||num>500000){
+                System.out.println(-1);
+            }
+            //至于这里count初始值为什么是-1我也不敢说也不敢问，反正我从0改为-1只有代码就过了[二哈]
+            //正解应该是i=1;count=0哈
+            int count = -1;
+            for(int i = 0;i<=num;i++){
+                int sum = 0;
+                for(int j = 1;j<i;j++){
+                    if(i%j == 0){
+                        sum += j;
+                    }
+                }
+                if(sum == i){
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+        sc.close();
+    }
 }
